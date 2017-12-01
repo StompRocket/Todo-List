b0750d80559d94d087834679ce590afd71ab42e5
ronan18 <Rosho123@users.noreply.github.com>
2017-11-30 18:47:26 -0800

    no data bug

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
 try {
-var app = new Vue({
-  el: '#app',
-  data: {
-    newItem: '',
-    items: []
-  },
+  var app = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      items: []
+    },
 
-  methods: {
-    init: function () {
-       if (localStorage.getItem("todoList")) {
-          app.items = JSON.parse(localStorage.getItem("todoList"))
-      } else {
-        window.alert('no data')
-        app.items.push({message: 'Welcome To List',done: false});
-        app.items.push({message: 'Bar',done: true})
-     }
-    },
-    update: function () {
-     localStorage.setItem("todoList", JSON.stringify(this.items))
-    },
-    newItemMethod: function () {
-      try {
-        this.items.push({
-          message: this.newItem,
-          done: false
-        })
-        this.newItem = ''
+    methods: {
+      init: function() {
+        if (localStorage.getItem('todoList')) {
+          app.items = JSON.parse(localStorage.getItem('todoList'))
+        } else {
+          app.items.push({
+            message: 'Welcome To List',
+            done: false
+          })
+          app.items.push({
+            message: 'Bar',
+            done: true
+          })
+        }
+      },
+      update: function() {
+        localStorage.setItem('todoList', JSON.stringify(this.items))
+      },
+      newItemMethod: function() {
+        try {
+          this.items.push({
+            message: this.newItem,
+            done: false
+          })
+          this.newItem = ''
+          app.update()
+        } catch (err) {
+          alert(err)
+        }
+      },
+      removeItem: function(item) {
+        var index = this.items.indexOf(item)
+        this.items.splice(index, 1)
         app.update()
-      } catch (err) {
-        alert(err)
       }
-    },
-    removeItem: function (item) {
-      var index = this.items.indexOf(item)
-      this.items.splice(index, 1)
-        app.update()
     }
-  }
-})
+  })
 
-window.onload = app.init;
+  window.onload = app.init
 } catch (err) {
-window.alert(err)
+  window.alert(err)
 }
