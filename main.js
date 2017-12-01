1f9ee58262be6478930b522adcafc977221fa25e
ronan18 <Rosho123@users.noreply.github.com>
2017-11-30 18:46:29 -0800

    Local storage

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
+try {
 var app = new Vue({
   el: '#app',
   data: {
     newItem: '',
-    items: [{
-      message: 'Foo'
-    },
-      {
-        message: 'Bar'
-      }
-    ]
+    items: []
   },
 
   methods: {
+    init: function () {
+       if (localStorage.getItem("todoList")) {
+          app.items = JSON.parse(localStorage.getItem("todoList"))
+      } else {
+        window.alert('no data')
+        app.items.push({message: 'Welcome To List',done: false});
+        app.items.push({message: 'Bar',done: true})
+     }
+    },
+    update: function () {
+     localStorage.setItem("todoList", JSON.stringify(this.items))
+    },
     newItemMethod: function () {
       try {
         this.items.push({
-          message: this.newItem
+          message: this.newItem,
+          done: false
         })
         this.newItem = ''
+        app.update()
       } catch (err) {
         alert(err)
       }
@@ -25,6 +34,12 @@ var app = new Vue({
     removeItem: function (item) {
       var index = this.items.indexOf(item)
       this.items.splice(index, 1)
+        app.update()
     }
   }
 })
+
+window.onload = app.init;
+} catch (err) {
+window.alert(err)
+}
